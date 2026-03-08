d91dd8acbe3add07c4f854345dcda3006b6163e6
unknown <mouryab30@gmail.com>
2026-03-09 02:06:58 +0530

    using cloudinary and multer for file sending

--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { createNewChat, getAllChat } from "../controllers/chat.js";
+import { createNewChat, getAllChat, getMessagesByChat, sendMessage } from "../controllers/chat.js";
+import { upload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.post('/chat/new', isAuth, createNewChat);
 router.get('/chat/all', isAuth, getAllChat);
+router.post('/message', isAuth, upload.single('image'), sendMessage);
+router.get('/message/:chatId', isAuth, getMessagesByChat);
 
 export default router;
