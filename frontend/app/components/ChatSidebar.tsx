a738f283f545fa0e1d38c3b799f8eebdfa550aa7
unknown <mouryab30@gmail.com>
2026-03-18 19:03:26 +0530

    chat side bar

--- frontend/app/components/ChatSidebar.tsx
+++ frontend/app/components/ChatSidebar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { User } from '../context/AppContext';
-import { CornerDownRight, CornerUpLeft, MessageCircle, Plus, Search, UserCircle, X } from 'lucide-react';
+import { CornerDownRight, CornerUpLeft, Link, LogOut, MessageCircle, Plus, Search, UserCircle, X } from 'lucide-react';
 
 interface ChatSidebarProps {
     sidebarOpen: boolean;
@@ -100,7 +100,7 @@ const ChatSidebar = ({ sidebarOpen, setShowAllUsers, setSidebarOpen, showAllUser
                                                 setSidebarOpen(false);
                                             }}
                                             className={`w-full text-left p-4 rounded-lg transition-colors ${isSelected ? "bg-blue-600 border border-blue-500"
-                                                : "border border-gray-700 hover: border-gray-600"
+                                                : "border border-gray-700 hover:border-gray-600"
                                                 }`}>
                                             <div className="flex items-center gap-3">
                                                 <div className="relative">
@@ -118,7 +118,7 @@ const ChatSidebar = ({ sidebarOpen, setShowAllUsers, setSidebarOpen, showAllUser
                                                             {chat.user.name}
                                                         </span>
                                                         {
-                                                            unseenCount > 0 && <div className="bg-red-600 text-white text-xs font-bold rounded-full min-w-[22px]
+                                                            unseenCount > 0 && <div className="bg-red-600 text-white text-xs font-bold rounded-full min-w-5.5
                                                                 h-5.5 flex items-center justify-center px-2">
                                                                 {unseenCount > 99 ? "99+" : unseenCount}
                                                             </div>
@@ -139,9 +139,29 @@ const ChatSidebar = ({ sidebarOpen, setShowAllUsers, setSidebarOpen, showAllUser
                             }
                         </div>
                     ) : (
-                        <div></div>
-                    )
-                }
+                        <div className="flex flex-col items-center justify-center text-center h-full">
+                            <div className="p-4 bg-gray-800 rounded-full mb-4">
+                                <MessageCircle className='w-8 h-8 text-gray-400' />
+                            </div>
+                            <p className="text-gray-400 font-medium">No conversation yet</p>
+                            <p className="text-sm text-gray-500 mt-1">
+                                Start a new chat to begin messaging
+                            </p>
+                        </div>
+                    )}
+            </div>
+            {/* footer */}
+            <div className="p-4 border-t border-gray-700 space-y-2">
+                <Link href={'/profile'} className='flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-gray-800 transition-colors'>
+                    <div className="p-1.5 bg-gray-700 rounded-lg"><UserCircle className='w-4 h-4 text-gray-300'/></div>
+                    <span className="font-medium text-gray-300">Profile</span>
+                </Link>
+
+                <button onClick={handleLogout} className='w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-red-600 transition-colors text-red-500
+                hover:text-white'>
+                    <div className="p-1.5 bg-red-600 rounded-lg"><LogOut className='w-4 h-4 text-gray-300'/></div>
+                    <span className="font-medium text-gray-300">Logout</span>
+                </button>
             </div>
         </aside>
     )
